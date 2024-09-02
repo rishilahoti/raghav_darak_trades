89adbb212c1426c4b0f8e8533b450fc0317f801e
Rishi <96910554+rishilahoti@users.noreply.github.com>
2024-09-02 16:27:05 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="StKnFVMdxYtC2pDJPSr7oQjfKmXrAl1UXRLLkokFj8o" />
         <meta name="description" content="Discover a wide variety of high-quality herbs, spices, and their powders. Perfect for culinary and medicinal uses." />
         <meta name="keywords" content="herbs, spices, powders, and raw materials" />
       </head>
